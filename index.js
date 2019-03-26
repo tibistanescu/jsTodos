f7b8686c8f5aa81f79b273a91e297f0e27829516
Tiberiu Stanescu <stanescutiberiugabriel@gmail.com>
2019-03-26 12:25:14 +0200

    move functionality in an object todoList

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-// it should have a place to store todos
-let todos = [];
-
-// it should have a way to display todos
-function displayTodos() {
-    console.log('My todos: ', todos);
+let todoList = {
+    // it should have a place to store todos
+    todos: [], 
+    // it should have a way to display todos
+    displayTodos: function(){ 
+        console.log('My todos:', this.todos);
+    },   
+    // it should have a way to add new todos
+    addTodo: function(todo){
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    // it should have a way to change a todo
+    changeTodo: function(position, newValue){
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    // it should have a way to delete a todo
+    deleteTodo: function(position){
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
 }
 
-// it should have a way to add new todos
-function addTodo() {
-    todos.push(todo);
-    displayTodos();    
-}
-
-// it should have a way to change a todo
-function changeTodo(position, newValue) {
-    todo[position] = newValue;
-    displayTodos();
-}
-
-// it should have a way to delete a todo
-function deleteTodo(position) {
-    todos.splice(position, 1);
-    displayTodos();
-}
-
+console.log(todoList);
+todoList.addTodo('test');
+todoList.addTodo('test2');
+todoList.changeTodo(0, 'test1');
+todoList.deleteTodo(1);
